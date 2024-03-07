7466e10e24a72ebff8cff829ac2d6a2ce4bd1528
Utkarshdesai <desaiutkarsh542@gmail.com>
2024-03-07 23:35:07 +0530

    notes

---
+++ intro.ts
@@ -0,0 +1,7 @@
+// To compile code run tsc file name run this cmd 
+console.log("hello world") 
+
+//data types 
+let user = "hello" 
+
+user = "yes" 
---
+++ variable.ts
@@ -0,0 +1,13 @@
+//syntax to write types also best pratice to type ts 
+//1. string 
+let user: string = "alex"  
+
+console.log(user)
+
+// it will throw error which number is assigned to string  
+//user = 9 
+
+console.log(user.length)
+// able to acess methods with string only not number like length,charat and many more 
+
+//2.number 
